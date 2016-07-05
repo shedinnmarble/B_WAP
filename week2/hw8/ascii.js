6efb287500fdf621f41c719e49ff31adc4f5f97a
Dewei Xiang <yafengxiang@163.com>
2016-07-05 15:22:58 -0500

    edit js animation lab

--- week2/hw8/ascii.js
+++ week2/hw8/ascii.js
@@ -1,62 +1,98 @@
+"use strict";
+var myAnmi = function() {
+    var timer;
+    var index = 0;
+    var speed = 250;
+    var animationData = ANIMATIONS["Dive"];
+    var text = document.getElementById("text");
+    var array = animationData.split("=====\n")
+    document.getElementById("stop").disabled = true;
 
-window.onload=function(){
-	var timer;
-	var index=0;
-	var speed=250;
-	var animationData=ANIMATIONS["Dive"];
-	var text=document.getElementById("text");
-	var array=animationData.split("=====\n")
-	function setText(){
-		timer=setInterval(function(){
-				text.value=array[index];
-				index++;
-				if(index>=array.length){
-					index=0;
-				}
-			},speed)
-	}	
-	function doAnimation(speed){
-		if(timer==null){
-			setText();
-		}else{
-			stop();
-			setText();
-		}
-	}
-	function stop(){
-		clearInterval(timer);
-		index=0;
-	}
-	document.getElementById("start").onclick=function(){
-		doAnimation(speed);
-		
-	}
-	document.getElementById("stop").onclick=function(){
-		stop();
-	}
-	document.getElementById("animation").onchange=function(){
-		var anmi=document.getElementById("animation");
-		var anmiText=anmi.options[anmi.selectedIndex].text;
-		animationData=ANIMATIONS[anmiText];
-		array=animationData.split("=====\n")
-		stop();
-		doAnimation(speed);
-	}
-	document.getElementById("size").onchange=function(){
-		var e = document.getElementById("size");
-		var fontSize = e.options[e.selectedIndex].value;
-		text.style.fontSize=fontSize;
-	}
-	document.getElementById("turbo").onclick=function(){
-		var checkbox=document.getElementById("turbo");
-		if(checkbox.checked){
-			speed=50;
-			stop();
-			doAnimation(speed);
-		}else{
-			speed=250;
-			stop();
-			doAnimation(speed);
-		}
-	}
+    function setText() {
+        timer = setInterval(function() {
+            text.value = array[index];
+            index++;
+            if (index >= array.length) {
+                index = 0;
+            }
+        }, speed)
+    }
+
+    function doAnimation(speed) {
+        if (timer == null) {
+            setText();
+        } else {
+            stop();
+            setText();
+        }
+        document.getElementById("stop").disabled = false;
+        document.getElementById("start").disabled = true;
+        document.getElementById("animation").disabled = true;
+        //document.getElementById("size").disabled=true;
+        //document.getElementById("turbo").disabled = false;
+    }
+
+    function stop() {
+        clearInterval(timer);
+        text.value=animationData;
+        document.getElementById("start").disabled = false;
+        document.getElementById("stop").disabled = true;
+        document.getElementById("animation").disabled = false;
+        document.getElementById("size").disabled = false;
+        //document.getElementById("turbo").disabled = true;
+    }
+
+    function changeAnimination() {
+        var anmi = document.getElementById("animation");
+        var anmiText = anmi.options[anmi.selectedIndex].text;
+        animationData = ANIMATIONS[anmiText];
+        array = animationData.split("=====\n");
+        text.value=animationData;
+    }
+
+    function changeSize() {
+        var e = document.getElementById("size");
+        var fontSize = e.options[e.selectedIndex].value;
+        text.style.fontSize = fontSize;
+    }
+    return {
+        startOnlick: function() {
+            changeAnimination();
+            changeSize();
+            doAnimation(speed);
+
+        },
+        stopOnclick: function() {
+            stop();
+        },
+        animationOnchange: function() {
+            changeAnimination();
+            //stop();
+            //doAnimation(speed);
+        },
+        sizeOnchange: function() {
+            changeSize();
+        },
+        turboOnclick: function() {
+            var checkbox = document.getElementById("turbo");
+            if (checkbox.checked) {
+                speed = 50;
+                stop();
+                doAnimation(speed);
+            } else {
+                speed = 250;
+                stop();
+                doAnimation(speed);
+            }
+        }
+    }
+
+}
+window.onload = function() {
+    var animation = myAnmi();
+    document.getElementById("start").onclick = animation.startOnlick;
+    document.getElementById("stop").onclick = animation.stopOnclick;
+    document.getElementById("animation").onchange = animation.animationOnchange;
+    document.getElementById("size").onchange = animation.sizeOnchange;
+    document.getElementById("turbo").onclick = animation.turboOnclick;
 }
--- week2/javascript/test.js
+++ week2/javascript/test.js
@@ -1,12 +1,38 @@
-var x=1;
-function f(){
-	var y=2;
-	var summ=function(){
-		var z=3;
-		console.log(x+y+z)
-	};
-	y=10;
-	return summ;
-}
-var g=f();
-g();
+// var x=1;
+// function f(){
+// 	var y=2;
+// 	var summ=function(){
+// 		var z=3;
+// 		console.log(x+y+z)
+// 	};
+// 	y=10;
+// 	return summ;
+// }
+// var g=f();
+// g();
+//clouse
+
+var makeCounter=(function(){
+	var privateCounter=0;
+	function changeBy(val){
+		privateCounter+=val;
+	}
+	return {
+		increment:function(){
+			changeBy(1)
+		},
+		decrement:function(){
+			changeBy(-1)
+		},
+		value:function(){
+			return privateCounter;
+		}
+	}
+})();
+var counter1=makeCounter;
+var counter2=makeCounter;
+counter1.increment();
+counter1.increment();
+counter1.increment();
+alert(counter1.privateCounter)
+alert(counter2.value())
