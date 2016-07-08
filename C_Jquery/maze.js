fd6feada0aea7ef5315ef850848e7131bd763d46
xiangdewei <yafengxiang@163.com>
2016-07-08 09:14:59 -0400

    finish Jquery game lab

--- C_Jquery/maze.js
+++ C_Jquery/maze.js
@@ -1,29 +1,39 @@
+"use strict";
 $(document).ready(function(){
 	var start=false;
+	var win=false;
 	$(".boundary").mouseover(function(){
-		$(this).addClass("youlose")
-		$("#status").text("you Lose!!");
-	})
-	$("#start").click(function(){
+		if(start){
+			$(this).addClass("youlose");
+			$("#status").text("you Lose!!");
+		}
+	});
+	$("#start").mouseover(function(){
 		// alert("start")
 		$(".boundary").removeClass("youlose");
-		$("#status").text("")
+		$("#status").text("Game start!");
 		start=true;
-	})
+		win=false;
+	});
 	$("#end").mouseover(function(){
 		var loser=$(".youlose").size();
 		// alert(loser);
-		if(loser>=1){
-			$("#status").text("You still lose")
-		}else{
-			$("#status").text("You Win");
+		if(start){
+			if(loser>=1){
+				$("#status").text("You still lose");
+			}else{
+				$("#status").text("You Win");
+				win=true;
+			}
 		}
 		// $("#end").
-	})
-	$("#maze").mouseout(function(){
+	});
+	$("#maze").mouseleave(function(){
 		if(start){
-			$("#status").text("You Lose！out of area")
-			$(".boundary").addClass("youlose")
+			if(!win){
+				$("#status").text("You Lose! out of area");
+				$(".boundary").addClass("youlose");
+			}
 		}
-	})
-})
+	});
+});
--- hw9_model_closuer/AccountExcerise.js
+++ hw9_model_closuer/AccountExcerise.js
@@ -1,26 +1,28 @@
 //create a module used to the button click event
 //some plcace to store the account list
-var accountList=new Array();
+var accountList=[];
 
 var AccountModule=(function(){
 	var create=function(){
 		var name=document.getElementById("name").value;
 		var deposit=document.getElementById("deposit").value;
-		accountList.push({name:name,deposit:deposit})
+		accountList.push({name:name,deposit:deposit});
 		display();
-	}
+	};
 	var display=function(){
 		//display this object to textarea
 		var content="";
-		for(var key in accountList){
-			content+="Account Name: "+accountList[key].name+" Balance "+accountList[key].deposit+"\r\n";
+		if(accountlist.length>0){
+			for(var key in accountList){
+				content+="Account Name: "+accountList[key].name+" Balance "+accountList[key].deposit+"\r\n";
+			}
+			document.getElementById("accountlist").value=content;
 		}
-		document.getElementById("accountlist").value=content;
-	}
+	};
 	return {
-		create:create
+		create:create;
 	}
 })();
 window.onload=function(){
-	document.getElementById("createAccount").onclick=AccountModule.create
+	document.getElementById("createAccount").onclick=AccountModule.create;
 }
